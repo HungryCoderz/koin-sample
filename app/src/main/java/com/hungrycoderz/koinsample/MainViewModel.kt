c8dd0376567863dc7ce28c4e9542630f2b22faf0
Rommel Azucena <jrommelazucena@gmail.com>
2019-10-07 08:22:32 +0800

    Prepare project source for a simple DI assignment

--- app/src/main/java/com/hungrycoderz/koinsample/MainViewModel.kt
+++ app/src/main/java/com/hungrycoderz/koinsample/MainViewModel.kt
@@ -4,16 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.koin.core.KoinComponent
-import org.koin.core.inject
 
 class MainViewModel : ViewModel(), KoinComponent {
 
-    private val weatherRepo: WeatherRepository by inject()
+    // TODO: Inject weather repository
 
     private val weatherStatus = MutableLiveData<String>()
     fun getWeatherStatus(): LiveData<String> = weatherStatus
 
     fun loadWeatherStatus() {
-        weatherStatus.value = weatherRepo.getWeatherStatus()
+        // TODO: Get weather status from weather repository
     }
 }
--- app/src/main/java/com/hungrycoderz/koinsample/MyApplication.kt
+++ app/src/main/java/com/hungrycoderz/koinsample/MyApplication.kt
@@ -1,23 +1,11 @@
 package com.hungrycoderz.koinsample
 
 import android.app.Application
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        setupKoin()
-    }
-
-    private fun setupKoin() {
-        val modules = module {
-            single { WeatherRepository() }
-        }
-
-        startKoin {
-            modules(modules)
-        }
+        // TODO: Setup Koin for dependency injection
     }
 }
